9b0ac6b920434bd2ef48e4fd914f4894221a3bee
eni041 <kimyerim5036@naver.com>
2023-12-18 11:54:01 +0900

    주문결제 페이지

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'goods_information_screen.dart';
 import 'ramen.dart';
 import 'screens/login_screen.dart';
+import 'payment_home.dart';
+
 
 
 void main() {
-  runApp(MyApp());
+  runApp(PaymentPage());
 }
